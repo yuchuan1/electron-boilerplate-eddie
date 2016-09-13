cf8f301a9e972b678eafb8953af834ca1b8728ec
Eddie Chen <yuchuan1@gmail.com>
2016-09-13 09:17:58 +0800

    add jsTree and bootstrap components

--- app/js/hello.js
+++ app/js/hello.js
@@ -10,6 +10,14 @@ var _reactDom2 = _interopRequireDefault(_reactDom);
 
 var _redux = require('redux');
 
+var _reactJsonTree = require('react-json-tree');
+
+var _reactJsonTree2 = _interopRequireDefault(_reactJsonTree);
+
+var _immutable = require('immutable');
+
+var _reactBootstrap = require('react-bootstrap');
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var initialState = {};
@@ -32,6 +40,15 @@ store.subscribe(function () {
 });
 store.dispatch({ type: 'SOME_ACTION', value: 'Hey, What\'s up?' });
 
+var json = {
+    array: [1, 2, 3],
+    bool: true,
+    object: {
+        foo: 'bar'
+    },
+    immutable: (0, _immutable.Map)({ key: 'value' })
+};
+
 var HelloMessage = _react2.default.createClass({
     displayName: 'HelloMessage',
 
@@ -46,9 +63,19 @@ var HelloMessage = _react2.default.createClass({
             this.state.value,
             ' ',
             this.props.name,
-            ' ',
             _react2.default.createElement('p', null),
-            ' Having fun with React and Redux? '
+            'Having fun with React and Redux?',
+            _react2.default.createElement(
+                _reactBootstrap.Button,
+                { bsStyle: 'primary', bsSize: 'large', block: true },
+                'Block level button'
+            ),
+            _react2.default.createElement(
+                _reactBootstrap.Button,
+                { bsSize: 'large', block: true },
+                'Block level button'
+            ),
+            _react2.default.createElement(_reactJsonTree2.default, { data: json })
         );
     }
 });
--- src/jsx/hello.jsx
+++ src/jsx/hello.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {createStore} from 'redux';
+import JSONTree from 'react-json-tree';
+import {Map} from 'immutable';
+import { Button } from 'react-bootstrap';
+//https://react-bootstrap.github.io/components.html
 
 let initialState = {};
 function reducer(state = initialState, action) {
@@ -17,15 +21,32 @@ let store = createStore(reducer);
 store.subscribe(() => console.log(store.getState()));
 store.dispatch({type: 'SOME_ACTION', value: 'Hey, What\'s up?'});
 
+const json = {
+    array: [
+        1, 2, 3
+    ],
+    bool: true,
+    object: {
+        foo: 'bar'
+    },
+    immutable: Map({key: 'value'})
+}
 
 var HelloMessage = React.createClass({
-  getInitialState: function() {
-      let state = store.getState();
-      return state;
-  },
-  render: function() {
-    return <div>{this.state.value} {this.props.name} <p/> Having fun with React and Redux? </div>;
-  }
+    getInitialState: function() {
+        let state = store.getState();
+        return state;
+    },
+    render: function() {
+        return <div>{this.state.value} {this.props.name}
+            <p/>
+            Having fun with React and Redux?
+            <Button bsStyle="primary" bsSize="large" block>Block level button</Button>
+            <Button bsSize="large" block>Block level button</Button>
+            <JSONTree data={json}/>
+            </div>;
+          }
 });
 
-ReactDOM.render(<HelloMessage name="Eddie" />, document.getElementById('main'));
+ReactDOM.render(
+    <HelloMessage name="Eddie"/>, document.getElementById('main'));
